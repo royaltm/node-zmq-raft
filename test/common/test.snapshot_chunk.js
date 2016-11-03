997800287452a2e6773c4bba086985ba584fe4f4
Rafał Michalski <royaltm75@gmail.com>
2016-11-03 09:20:34 +0100

    snapshot_chunk: isFirstChunk, isLastChunk properties

--- test/common/test.snapshot_chunk.js
+++ test/common/test.snapshot_chunk.js
@@ -17,36 +17,50 @@ test('should have functions and properties', t => {
 
 test('SnapshotChunk', t => {
   var buf = Buffer.from([0xc0]);
-  var req = new SnapshotChunk(buf, 1, 2, 3);
-  t.type(req, SnapshotChunk);
-  t.type(req, Buffer);
-  t.notStrictEquals(req, buf);
-  t.strictEquals(req.buffer, buf.buffer);
-  t.strictEquals(req.byteOffset, buf.byteOffset);
-  t.strictEquals(req.byteLength, buf.byteLength);
-  t.strictEquals(SnapshotChunk.isSnapshotChunk(req), true);
+  var chunk = new SnapshotChunk(buf, 1, 2, 3);
+  t.type(chunk, SnapshotChunk);
+  t.type(chunk, Buffer);
+  t.notStrictEquals(chunk, buf);
+  t.strictEquals(chunk.buffer, buf.buffer);
+  t.strictEquals(chunk.byteOffset, buf.byteOffset);
+  t.strictEquals(chunk.byteLength, buf.byteLength);
+  t.strictEquals(chunk.isFirstChunk, false);
+  t.strictEquals(chunk.isLastChunk, true);
+  t.strictEquals(SnapshotChunk.isSnapshotChunk(chunk), true);
   t.strictEquals(SnapshotChunk.isSnapshotChunk(buf), false);
-  t.strictEquals(req.isSnapshotChunk, true);
+  t.strictEquals(chunk.isSnapshotChunk, true);
   t.strictEquals(buf.isSnapshotChunk, undefined);
-  t.strictEquals(req.logIndex, 1);
-  t.strictEquals(req.snapshotByteOffset, 2);
-  t.strictEquals(req.snapshotTotalLength, 3);
+  t.strictEquals(chunk.logIndex, 1);
+  t.strictEquals(chunk.snapshotByteOffset, 2);
+  t.strictEquals(chunk.snapshotTotalLength, 3);
+
+  chunk = new SnapshotChunk(buf, 1, 0, 1);
+  t.notStrictEquals(chunk, buf);
+  t.strictEquals(chunk.isFirstChunk, true);
+  t.strictEquals(chunk.isLastChunk, true);
+
+  chunk = new SnapshotChunk(buf, 1, 2, 4);
+  t.notStrictEquals(chunk, buf);
+  t.strictEquals(chunk.isFirstChunk, false);
+  t.strictEquals(chunk.isLastChunk, false);
 
   t.test('bufferToSnapshotChunk', t => {
     var buf = Buffer.from([0xc0]);
-    var req = SnapshotChunk.bufferToSnapshotChunk(buf, 1, 2, 3);
-    t.type(req, SnapshotChunk);
-    t.type(req, Buffer);
-    t.strictEquals(req, buf);
-    t.strictEquals(SnapshotChunk.isSnapshotChunk(req), true);
+    var chunk = SnapshotChunk.bufferToSnapshotChunk(buf, 1, 0, 3);
+    t.type(chunk, SnapshotChunk);
+    t.type(chunk, Buffer);
+    t.strictEquals(chunk, buf);
+    t.strictEquals(chunk.isFirstChunk, true);
+    t.strictEquals(chunk.isLastChunk, false);
+    t.strictEquals(SnapshotChunk.isSnapshotChunk(chunk), true);
     t.strictEquals(SnapshotChunk.isSnapshotChunk(buf), true);
-    t.strictEquals(req.isSnapshotChunk, true);
+    t.strictEquals(chunk.isSnapshotChunk, true);
     t.strictEquals(buf.isSnapshotChunk, true);
-    t.strictEquals(req.logIndex, 1);
-    t.strictEquals(req.snapshotByteOffset, 2);
-    t.strictEquals(req.snapshotTotalLength, 3);
+    t.strictEquals(chunk.logIndex, 1);
+    t.strictEquals(chunk.snapshotByteOffset, 0);
+    t.strictEquals(chunk.snapshotTotalLength, 3);
     t.strictEquals(buf.logIndex, 1);
-    t.strictEquals(buf.snapshotByteOffset, 2);
+    t.strictEquals(buf.snapshotByteOffset, 0);
     t.strictEquals(buf.snapshotTotalLength, 3);
     t.end();
   });
