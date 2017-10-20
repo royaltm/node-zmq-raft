691e046c488aca7af32568f71c43b8f2c5e482da
Rafał Michalski <royaltm75@gmail.com>
2017-10-20 11:35:55 +0200

    RaftPersistence: peersUpdateRequest added

--- test/server/test.raft_persistence.js
+++ test/server/test.raft_persistence.js
@@ -26,13 +26,14 @@ test('should be a function', t => {
 test('RaftPersistence', suite => {
 
   suite.test('test new persistence', t => {
-    t.plan(24);
+    t.plan(29);
     var persistence = new RaftPersistence(path.join(workdir, 'one.persist'), []);
     t.type(persistence, RaftPersistence);
     t.type(persistence.ready, 'function');
     return persistence.ready().then(() => {
       t.strictEquals(persistence.currentTerm, 0);
       t.strictEquals(persistence.votedFor, null);
+      t.strictEquals(persistence.peersUpdateRequest, null);
       t.deepEquals(persistence.peers, []);
       t.strictEquals(persistence[Symbol.for('byteSize')], 0);
 
@@ -40,6 +41,7 @@ test('RaftPersistence', suite => {
     }).then(() => {
       t.strictEquals(persistence.currentTerm, 1);
       t.strictEquals(persistence.votedFor, null);
+      t.strictEquals(persistence.peersUpdateRequest, null);
       t.deepEquals(persistence.peers, []);
       t.strictEquals(persistence[Symbol.for('byteSize')], 14);
 
@@ -47,6 +49,7 @@ test('RaftPersistence', suite => {
     }).then(() => {
       t.strictEquals(persistence.currentTerm, 1);
       t.strictEquals(persistence.votedFor, 'me');
+      t.strictEquals(persistence.peersUpdateRequest, null);
       t.deepEquals(persistence.peers, []);
       t.strictEquals(persistence[Symbol.for('byteSize')], 27);
 
@@ -54,78 +57,84 @@ test('RaftPersistence', suite => {
     }).then(() => {
       t.strictEquals(persistence.currentTerm, 2);
       t.strictEquals(persistence.votedFor, null);
+      t.strictEquals(persistence.peersUpdateRequest, null);
       t.deepEquals(persistence.peers, []);
       t.strictEquals(persistence[Symbol.for('byteSize')], 51);
 
       return Promise.all([
         persistence.update({currentTerm: 3, votedFor: 'him'}),
-        persistence.update({currentTerm: 4, peers: ['foo','bar','baz']})
+        persistence.update({currentTerm: 4, peers: ['foo','bar','baz'], peersUpdateRequest: 'AAAAAAAAAAAAAAAA'})
       ]);
     }).then(() => {
       t.strictEquals(persistence.currentTerm, 4);
       t.strictEquals(persistence.votedFor, 'him');
+      t.strictEquals(persistence.peersUpdateRequest, 'AAAAAAAAAAAAAAAA');
       t.deepEquals(persistence.peers, ['foo','bar','baz']);
-      t.strictEquals(persistence[Symbol.for('byteSize')], 111);
+      t.strictEquals(persistence[Symbol.for('byteSize')], 147);
 
       return persistence.close();
     }).then(() => {
-      t.strictEquals(fs.statSync(persistence.filename).size, 111);
+      t.strictEquals(fs.statSync(persistence.filename).size, 147);
     })
     .then(() => t.ok(true)).catch(t.threw);
   });
 
   suite.test('test existing persistence', t => {
-    t.plan(12);
+    t.plan(14);
     var persistence = new RaftPersistence(path.join(workdir, 'one.persist'), []);
     t.type(persistence, RaftPersistence);
     t.type(persistence.ready, 'function');
     return persistence.ready().then(() => {
       t.strictEquals(persistence.currentTerm, 4);
       t.strictEquals(persistence.votedFor, 'him');
+      t.strictEquals(persistence.peersUpdateRequest, 'AAAAAAAAAAAAAAAA');
       t.deepEquals(persistence.peers, ['foo','bar','baz']);
-      t.strictEquals(persistence[Symbol.for('byteSize')], 111);
+      t.strictEquals(persistence[Symbol.for('byteSize')], 147);
 
       return persistence.update({votedFor: 'foo', currentTerm: 5});
     }).then(() => {
       t.strictEquals(persistence.currentTerm, 5);
       t.strictEquals(persistence.votedFor, 'foo');
+      t.strictEquals(persistence.peersUpdateRequest, 'AAAAAAAAAAAAAAAA');
       t.deepEquals(persistence.peers, ['foo','bar','baz']);
-      t.strictEquals(persistence[Symbol.for('byteSize')], 138);
+      t.strictEquals(persistence[Symbol.for('byteSize')], 174);
 
       return persistence.close();
     }).then(() => {
-      t.strictEquals(fs.statSync(persistence.filename).size, 138);
+      t.strictEquals(fs.statSync(persistence.filename).size, 174);
     })
     .then(() => t.ok(true)).catch(t.threw);
   });
 
   suite.test('test rotate', t => {
-    t.plan(12);
+    t.plan(14);
     var persistence = new RaftPersistence(path.join(workdir, 'one.persist'), []);
     t.type(persistence, RaftPersistence);
     t.type(persistence.ready, 'function');
     return persistence.ready().then(() => {
       t.strictEquals(persistence.currentTerm, 5);
       t.strictEquals(persistence.votedFor, 'foo');
+      t.strictEquals(persistence.peersUpdateRequest, 'AAAAAAAAAAAAAAAA');
       t.deepEquals(persistence.peers, ['foo','bar','baz']);
-      t.strictEquals(persistence[Symbol.for('byteSize')], 138);
+      t.strictEquals(persistence[Symbol.for('byteSize')], 174);
 
       return persistence.rotate({currentTerm: 6});
     }).then(() => {
       t.strictEquals(persistence.currentTerm, 6);
       t.strictEquals(persistence.votedFor, 'foo');
+      t.strictEquals(persistence.peersUpdateRequest, 'AAAAAAAAAAAAAAAA');
       t.deepEquals(persistence.peers, ['foo','bar','baz']);
-      t.strictEquals(persistence[Symbol.for('byteSize')], 46);
+      t.strictEquals(persistence[Symbol.for('byteSize')], 82);
 
       return persistence.close();
     }).then(() => {
-      t.strictEquals(fs.statSync(persistence.filename).size, 46);
+      t.strictEquals(fs.statSync(persistence.filename).size, 82);
     })
     .then(() => t.ok(true)).catch(t.threw);
   });
 
   suite.test('test auto rotate', t => {
-    t.plan(6 + 4 + 2);
+    t.plan(7 + 5 + 2);
     var bigString = Buffer.allocUnsafe(64*1024).toString('hex');
     var persistence = new RaftPersistence(path.join(workdir, 'one.persist'), []);
     t.type(persistence, RaftPersistence);
@@ -133,8 +142,9 @@ test('RaftPersistence', suite => {
     return persistence.ready().then(() => {
       t.strictEquals(persistence.currentTerm, 6);
       t.strictEquals(persistence.votedFor, 'foo');
+      t.strictEquals(persistence.peersUpdateRequest, 'AAAAAAAAAAAAAAAA');
       t.deepEquals(persistence.peers, ['foo','bar','baz']);
-      t.strictEquals(persistence[Symbol.for('byteSize')], 46);
+      t.strictEquals(persistence[Symbol.for('byteSize')], 82);
 
       var bytesize = 0;
       var expectedTerm = 11;
@@ -144,15 +154,16 @@ test('RaftPersistence', suite => {
           return persistence.update({currentTerm: persistence.currentTerm + 1, votedFor: bigString}).then(next);
         } else {
           t.strictEquals(persistence.currentTerm, expectedTerm);
-          t.strictEquals(persistence[Symbol.for('byteSize')], 131119);
+          t.strictEquals(persistence[Symbol.for('byteSize')], 131155);
           t.strictEquals(persistence.votedFor, bigString);
+          t.strictEquals(persistence.peersUpdateRequest, 'AAAAAAAAAAAAAAAA');
           t.deepEquals(persistence.peers, ['foo','bar','baz']);
           return persistence.close();
         }
       };
       return next();
     }).then(() => {
-      t.strictEquals(fs.statSync(persistence.filename).size, 131119);
+      t.strictEquals(fs.statSync(persistence.filename).size, 131155);
     })
     .then(() => t.ok(true)).catch(t.threw);
   });
