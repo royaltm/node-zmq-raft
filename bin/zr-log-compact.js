16a7106661b302dbb0c200bec6c03482d2f27074
Rafał Michalski <royaltm75@gmail.com>
2018-01-07 20:11:39 +0100

    zr-log-compact: state-machine relative to config directory, better handling of zip option

--- bin/zr-log-compact.js
+++ bin/zr-log-compact.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* 
- *  Copyright (c) 2016-2017 Rafał Michalski <royal@yeondir.com>
+ *  Copyright (c) 2016-2018 Rafał Michalski <royal@yeondir.com>
  */
 "use strict";
 
@@ -10,7 +10,7 @@ const path = require('path')
     , fs = require('fs')
     , assert = require('assert');
 
-const { Z_BEST_COMPRESSION } = require('zlib');
+const { Z_NO_COMPRESSION, Z_BEST_COMPRESSION } = require('zlib');
 
 const program = require('commander')
     , Hjson = require('hjson')
@@ -23,15 +23,15 @@ const raft = require('..');
 const { server: { FileLog, builder: { createOptions } }
       , common: { SnapshotFile }
       , client: { ZmqRaftClient }
-      , utils: { fsutil: { mkdirp }, tempfiles: { createTempName, cleanupTempFiles } }
+      , utils: { fsutil: { mkdirp }
+               , tempfiles: { createTempName, cleanupTempFiles }
+               , helpers: { parsePeers } }
       } = raft;
-
-const defaultConfig = path.join(__dirname, '..', 'config', 'default.hjson');
 
 program
   .version(pkg.version)
   .usage('[options]')
-  .option('-c, --config <file>', 'Config file', defaultConfig)
+  .option('-c, --config <file>', 'Config file in hjson format')
   .option('-t, --target <file>', 'Target snapshot filename')
   .option('-m, --state-machine <file>', 'State machine')
   .option('-p, --peer <url>', 'Peer url or id to determine last index from commit index')
@@ -43,104 +43,152 @@ program
   .option('-U, --no-unzip', 'State machine unzipSnapshot option')
   .parse(process.argv);
 
-if (!isFinite(program.zip)) program.zip = Z_BEST_COMPRESSION;
+function mergeSmOptions(options) {
+  var smOptions = Object.assign({compressionLevel: Z_BEST_COMPRESSION}, options);
 
-const options = readConfig(program.config);
-
-const rootDir = program.dir || options.data.path;
-const logDir = path.resolve(rootDir, program.log || options.data.log || 'log');
-const snapFile = path.resolve(rootDir, program.snapshot || options.data.snapshot || 'snap');
-
-const targetFile = program.target ||
-                   (options.data.compact.install && path.resolve(rootDir, options.data.compact.install));
-const stateMachinePath = program.stateMachine ||
-                         (options.data.compact.state.path && path.resolve(rootDir, options.data.compact.state.path));
-
-const lastIndex = program.index;
-const peerUrl = program.peer; // TODO peer id
-
-if (!targetFile) {
-  console.error("no target file");
-  process.exit(2);
-}
-
-if (!stateMachinePath) {
-  console.error("no state machine specified");
-  process.exit(3);
-}
-
-var lastIndexPromise;
-if (isFinite(lastIndex) && lastIndex > 0) {
-  lastIndexPromise = Promise.resolve(lastIndex);
-}
-else if (peerUrl) {
-  const client = new ZmqRaftClient({peers: [peerUrl]});
-  lastIndexPromise = client.requestLogInfo(true, 5000).then(({commitIndex}) => {
-    client.close();
-    return commitIndex;
-  });
-}
-else {
-  console.error("specify last index or peer url");
-  process.exit(4);
-}
-
-const StateMachineClass = loadState(stateMachinePath);
-
-const stateMachine = new StateMachineClass(Object.assign({}, options.data.compact.state.options, {
-  compressionLevel: program.zip,
-  unzipSnapshot: program.unzip
-}));
-const fileLog = new FileLog(logDir, snapFile, true);
-
-Promise.all([
-  fileLog.ready(), lastIndexPromise, mkdirp(path.dirname(targetFile))
-])
-.then(([fileLog, lastIndex]) => Promise.all([
-  fileLog.ready(), stateMachine.ready(), lastIndex, fileLog.termAt(lastIndex)
-]))
-.then(([fileLog, stateMachine, lastIndex, lastTerm]) => {
-  var tempSnapshot;
-
-  if (lastTerm === undefined) throw new Error("last index not found in the file log");
-
-  if (stateMachine.snapshotReadStream) {
-    tempSnapshot = new SnapshotFile(createTempName(targetFile), lastIndex, lastTerm, stateMachine.snapshotReadStream);
+  /* force zip */
+  if (program.zip !== undefined) {
+    smOptions.compressionLevel = program.zip;
   }
 
-  debug('feeding state machine with log data indexes: %s - %s', stateMachine.lastApplied + 1, lastIndex);
+  if ('number' !== typeof smOptions.compressionLevel
+      || isNaN(smOptions.compressionLevel)
+      || smOptions.compressionLevel < 0
+      || smOptions.compressionLevel > Z_BEST_COMPRESSION) {
+    throw new TypeError('zip level must be an integer from 0 to ' + Z_BEST_COMPRESSION);
+  }
 
-  return fileLog.feedStateMachine(stateMachine, lastIndex)
-  .then(lastApplied => {
-    assert.strictEqual(lastIndex, lastApplied);
+  if (program.unzip === false) {
+    /* force no-unzip */
+    smOptions.unzipSnapshot = false;
+  }
+  else if (smOptions.unzipSnapshot === undefined
+          && options.compressionLevel !== Z_NO_COMPRESSION) {
+    /* set unzip if not set on original options
+       and original zip level wasn't no-compression */
+    smOptions.unzipSnapshot = true;
+  }
 
-    var tempSnapshotPromise;
+  return smOptions;
+}
 
-    if (!tempSnapshot) {
-      if ('function' === typeof stateMachine.createSnapshotReadStream) {
-        tempSnapshot = new SnapshotFile(createTempName(targetFile), lastIndex, lastTerm, stateMachine.createSnapshotReadStream());
-        tempSnapshotPromise = tempSnapshot.ready();
-      }
-      else if ('function' === typeof stateMachine.serialize) {
-        const snapshotData = stateMachine.serialize();
-        tempSnapshot = new SnapshotFile(createTempName(targetFile), lastIndex, lastTerm, snapshotData.length);
-        tempSnapshotPromise = tempSnapshot.ready().then(s => s.write(snapshotData, 0, snapshotData.length));
+function findPeerUrl(options) {
+  var peers = parsePeers(options.peers);
+  if ('string' === typeof options.id) {
+    return peers.get(options.id);
+  }
+}
+
+function exitError(status) {
+  args = [].slice.call(arguments, 1);
+  console.error.apply(console, args);
+  process.exit(status);
+}
+
+function start() {
+
+  const options = readConfig(program.config);
+
+  const rootDir = program.dir || options.data.path;
+
+  if ('string' !== typeof rootDir) {
+    throw new TypeError('data directory must be defined!');
+  }
+
+  debug('data directory: %s', rootDir);
+
+  const configDir = program.config ? path.dirname(path.resolve(program.config))
+                                   : rootDir;
+
+  const logDir = path.resolve(rootDir, program.log || options.data.log || 'log');
+  const snapFile = path.resolve(rootDir, program.snapshot || options.data.snapshot || 'snap');
+
+  const targetFile = program.target ||
+                     (options.data.compact.install && path.resolve(rootDir, options.data.compact.install));
+  const stateMachinePath = program.stateMachine ||
+                           (options.data.compact.state.path && path.resolve(configDir, options.data.compact.state.path));
+
+  if (!targetFile) {
+    exitError(2, "no target file");
+  }
+
+  debug('target snapshot: %s', targetFile);
+
+  if (!stateMachinePath) {
+    exitError(3, "no state machine specified");
+  }
+
+  var lastIndexPromise;
+  if (isFinite(program.index) && program.index > 0) {
+    lastIndexPromise = Promise.resolve(program.index);
+  }
+  else {
+    const peerUrl = program.peer || findPeerUrl(options);
+    if (!peerUrl) {
+      exitError(4, "specify last index or peer url");
+    }
+    const client = new ZmqRaftClient({peers: [peerUrl]});
+    lastIndexPromise = client.requestLogInfo(true, 5000).then(({commitIndex, pruneIndex}) => {
+      client.close();
+      return Math.min(commitIndex, pruneIndex);
+    });
+  }
+
+  const StateMachineClass = loadState(stateMachinePath);
+
+  const stateMachine = new StateMachineClass(mergeSmOptions(options.data.compact.state.options));
+  const fileLog = new FileLog(logDir, snapFile, true);
+
+  return Promise.all([
+    fileLog.ready(), lastIndexPromise, mkdirp(path.dirname(targetFile))
+  ])
+  .then(([fileLog, lastIndex]) => Promise.all([
+    fileLog.ready(), stateMachine.ready(), lastIndex, fileLog.termAt(lastIndex)
+  ]))
+  .then(([fileLog, stateMachine, lastIndex, lastTerm]) => {
+    var tempSnapshot;
+
+    if (lastTerm === undefined) throw new Error("last index not found in the file log");
+
+    if (stateMachine.snapshotReadStream) {
+      tempSnapshot = new SnapshotFile(createTempName(targetFile), lastIndex, lastTerm, stateMachine.snapshotReadStream);
+    }
+
+    debug('feeding state machine with log data indexes: %s - %s', stateMachine.lastApplied + 1, lastIndex);
+
+    return fileLog.feedStateMachine(stateMachine, lastIndex)
+    .then(lastApplied => {
+      assert.strictEqual(lastIndex, lastApplied);
+
+      var tempSnapshotPromise;
+
+      if (!tempSnapshot) {
+        if ('function' === typeof stateMachine.createSnapshotReadStream) {
+          tempSnapshot = new SnapshotFile(createTempName(targetFile), lastIndex, lastTerm, stateMachine.createSnapshotReadStream());
+          tempSnapshotPromise = tempSnapshot.ready();
+        }
+        else if ('function' === typeof stateMachine.serialize) {
+          const snapshotData = stateMachine.serialize();
+          tempSnapshot = new SnapshotFile(createTempName(targetFile), lastIndex, lastTerm, snapshotData.length);
+          tempSnapshotPromise = tempSnapshot.ready().then(s => s.write(snapshotData, 0, snapshotData.length));
+        }
+        else {
+          throw new Error("Could not determine how to create a snapshot data from the provided state machine.");
+        }
       }
       else {
-        throw new Error("Could not determine how to create a snapshot data from the provided state machine.");
+        tempSnapshotPromise = tempSnapshot.ready();
       }
-    }
-    else {
-      tempSnapshotPromise = tempSnapshot.ready();
-    }
-    return Promise.all([tempSnapshotPromise, stateMachine.close()]);
+      return Promise.all([tempSnapshotPromise, stateMachine.close()]);
+    })
+    .then(() => tempSnapshot.replace(targetFile))
+    .then(() => cleanupTempFiles(targetFile, debug))
+    .then(() => tempSnapshot.close());
   })
-  .then(() => tempSnapshot.replace(targetFile))
-  .then(() => cleanupTempFiles(targetFile, debug))
-  .then(() => tempSnapshot.close());
-})
-.then(() => debug('compaction done'))
-.catch(err => {
+  .then(() => debug('compaction done'));
+}
+
+Promise.resolve().then(start).catch(err => {
   console.error("LOG COMPACTION ERROR");
   console.error(err.stack);
   process.exit(1);
@@ -155,6 +203,7 @@ function loadState(filename) {
 function readConfig(configFile) {
   var config;
   if (configFile) {
+    debug('reading config: %s', configFile);
     const text = fs.readFileSync(configFile, 'utf8');
     config = Hjson.parse(text);
   }
