d80d42360abc1886e1f8b52ac6d99e6c01dcd52a
Rafał Michalski <royaltm75@gmail.com>
2018-01-10 22:33:29 +0100

    console: some helpers moved to lib

--- bin/console.js
+++ bin/console.js
@@ -14,13 +14,12 @@ const path = require('path');
 const util = require('util');
 const crypto = require('crypto');
 const { createUnzip } = require('zlib');
+
 const debug = require('debug')('zmq-raft:console');
 
 const isArray = Array.isArray;
 
 const pkg = require('../package.json');
-
-const tmpdir = path.normalize(path.join(__dirname, '..', 'tmp'));
 
 const ben = require('ben');
 
@@ -35,10 +34,14 @@ const { genIdent } = require('../lib/utils/id');
 const { createRepl } = require('../lib/utils/repl');
 const ZmqRaftClient = require('../lib/client/zmq_raft_client');
 const ZmqRaftSubscriber = require('../lib/client/zmq_raft_subscriber');
-const { lpad, regexpEscape } = require('../lib/utils/helpers');
-const { createRotateName } = require('../lib/utils/filerotate');
 const { LOG_ENTRY_TYPE_STATE, LOG_ENTRY_TYPE_CONFIG, LOG_ENTRY_TYPE_CHECKPOINT
       , readers: { readTypeOf, readTermOf, readDataOf } } = require('../lib/common/log_entry');
+
+const { listPeers
+      , showInfo
+      , argToBoolean
+      , prompt
+      , replError } = require('../lib/utils/repl_helpers');
 
 const argv = process.argv.slice(2);
 
@@ -364,56 +367,6 @@ createRepl().then(repl => {
 
 }).catch(err => console.warn(err.stack));
 
-function listPeers(client) {
-  if (!client) {
-    console.log(yellow('not connected'));
-    return Promise.resolve();
-  }
-  return client.requestConfig(5000).then(peers => {
-    console.log(magenta('Cluster peers:'))
-    for(let id in peers.urls) {
-      let url = peers.urls[id];
-      if (id === peers.leaderId) {
-        console.log(bgGreen(`${id}: ${url}`));
-      }
-      else
-        console.log(`${cyan(id)}: ${grey(url)}`);
-    }
-  });
-}
-
-function showInfo(client, id) {
-  if (!client) {
-    console.log(yellow('not connected'));
-    return Promise.resolve();
-  }
-  var cleanUp, anyPeer = false;
-  if (client.peers.has(id)) {
-    client = new ZmqRaftClient({peers: [[id, client.peers.get(id)]]});
-    cleanUp = () => client.close();
-    anyPeer = true;
-  }
-  else if (id) {
-    console.log(yellow('unknown id: %s'), id);
-    return Promise.resolve();
-  }
-  else cleanUp = () => {};
-  return client.requestLogInfo(anyPeer, 5000)
-  .then(({isLeader, leaderId, currentTerm, firstIndex, lastApplied, commitIndex, lastIndex, snapshotSize, pruneIndex}) => {
-    if (!anyPeer) assert(isLeader);
-    console.log(grey(`Log information for: "${isLeader ? green(leaderId) : yellow(id)}"`));
-    console.log(`leader:          ${isLeader ? green('yes') : cyan('no')}`);
-    console.log(`current term:    ${magenta(lpad(currentTerm, 14))}`);
-    console.log(`first log index: ${magenta(lpad(firstIndex, 14))}`);
-    console.log(`last applied:    ${magenta(lpad(lastApplied, 14))}`);
-    console.log(`commit index:    ${magenta(lpad(commitIndex, 14))}`);
-    console.log(`last log index:  ${magenta(lpad(lastIndex, 14))}`);
-    console.log(`snapshot size:   ${magenta(lpad(snapshotSize, 14))}`);
-    console.log(`prune index:     ${magenta(lpad(pruneIndex, 14))}`);
-  })
-  .then(cleanUp, err => { cleanUp(); throw err; });
-}
-
 function floodingNextFactory(enc, flood) {
   return function() {
     if (flood.flooding) {
@@ -463,32 +416,4 @@ function startFloodingStream(repl, subs) {
 
   flood.flooding = true;
   next();
-}
-
-function argToBoolean(arg) {
-  switch(arg.toLowerCase()) {
-  case 'yes':
-  case 'y':
-  case 'on':
-  case '1':
-    return true;
-  default:
-    return false;
-  }
-}
-
-function prompt(repl) {
-  repl.lineParser && repl.lineParser.reset();
-  if (repl.clearBufferedCommand) {
-    repl.clearBufferedCommand();
-  } else {
-    repl.bufferedCommand = '';
-  }
-  repl.displayPrompt();
-};
-
-function replError(repl, err) {
-  console.warn(red('ERROR'));
-  console.warn(err.stack);
-  prompt(repl);
 }
